2643577fa6bb68cc8629a9d04868841ce65df4f7
Elias Bernhaut <elias.bernhaut@novu.ch>
2025-04-22 21:42:37 +0200

    feat: add kulala

--- init.lua
+++ init.lua
@@ -1093,6 +1093,7 @@ require('lazy').setup({
   require 'custom.plugins.markview',
   require 'custom.plugins.neotest',
   require 'custom.plugins.git_fugitive',
+  require 'custom.plugins.kulala',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
